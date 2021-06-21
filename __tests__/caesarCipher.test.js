1ac4b9c237077f4e403f8cf81b7fac1169456406
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-06-21 12:07:27 +0100

    caesarCipher shifts characters by given amount

--- __tests__/caesarCipher.test.js
+++ __tests__/caesarCipher.test.js
@@ -8,4 +8,9 @@ describe('caesarCipher', () => {
   it('receives a string and shift value to return a new string', () => {
     expect(typeof caesarCipher('foobar', 1)).toBe('string');
   });
+
+  it('shifts alphabetical characters by given amount', () => {
+    expect(caesarCipher('foo', 1)).toBe('gpp');
+    expect(caesarCipher('foo', 2)).toBe('hqq');
+  });
 });
--- src/encryptors.js
+++ src/encryptors.js
@@ -1,5 +1,10 @@
 const caesarCipher = (str, amount = 0) => {
-  return str;
+  const encryptedMessage = str.split('').map((char, index) => {
+    const code = str.charCodeAt(index);
+    const shiftedCode = code + amount;
+    return String.fromCharCode(shiftedCode);
+  });
+  return encryptedMessage.join('');
 };
 
 module.exports.caesarCipher = caesarCipher;
